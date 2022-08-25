5df03c950ca5c799ee330fa3415600ac0841754b
axeman <axeman.ofic@gmail.com>
2022-08-25 13:09:10 +0300

    fixed tests

--- tests/test_send_message.py
+++ tests/test_send_message.py
@@ -2,15 +2,17 @@ import os
 
 from dotenv import load_dotenv
 
-from tnt_python.types import MessageData
+from tnt_python.data import MessageData
 from tnt_python.services import NotifyerService
+from tnt_python.types import ResponseSuccessType, ResponseType
 
 
 def test_send_message():
     load_dotenv()
-    token = os.getenv("TG_TOKEN")
-    data = MessageData(
+    token: str = os.getenv("TG_TOKEN")
+    data: MessageData = MessageData(
         chat_id=os.getenv("CHAT_ID"),
         text="Hello world"
     )
-    assert NotifyerService.send_message(data, token) == 200
+    response: ResponseType = NotifyerService().send_message(data, token)
+    assert response.status_code == 200 and isinstance(response.result, ResponseSuccessType)
